f040257a45d091f98310688c7efb87a06ae8b5a8
= <nerd1001001@gmail.com>
2019-11-07 02:39:39 -0600

    __add and restructuring
    Added the first math metamethod and restructured the metatable for the
    matrices

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# lua_matrix 
+# lua\_matrix 
 A matrix library for Lua, written in C. (This is basically a port of a library I've already written in Lua, but am using to learn the C api.)
 
 # Todo:
@@ -6,12 +6,28 @@ A matrix library for Lua, written in C. (This is basically a port of a library I
 - [x] Basic Constructor
 - [x] Basic Getters and Setters
 - [x] Basic error checking for index out of range
+- [ ] Either a \_\_tostring method or a simple print method to help debugging
 - [ ] Add aritmetic metamethods
+	- [x] \_\_add
+	- [ ] \_\_sub
+	- [ ] \_\_unm
+	- [ ] \_\_mul (the important one)
+		- [ ] Matrix Matrix product
+		- [ ] Matrix Number product
+	- [ ] \_\_div
+	- [ ] \_\_idiv
+	- [ ] \_\_mod (? probably mod each element)
+	- [ ] \_\_pow (for integers)
 - [ ] Add metamethods to allow access of matrices using index notation (i.e. a[2][3] instead of a:get(2,3))
 - Different constructors for
 	- [ ] Arbitrary Matrix initialized with a table of tables
+	- [ ] Arbitrary Matrix initialized with a table of numbers and an int of rows
 	- [ ] Identity Matrix
 	- [ ] Random Matrix
+- [ ] Iterators
+	- [ ] rows - each call returns a row as a table
+	- [ ] columns - each call returns a column as a table
+	- [ ] entries - each call g
 - [ ] Map function to apply function to each element of a matrix
 
 
@@ -19,9 +35,11 @@ A matrix library for Lua, written in C. (This is basically a port of a library I
 - [ ] Determinant method
 - [ ] Row reduction and Gaussian Elimination methods
 - [ ] Implement the Aguilera-Perez Algorithm for Rotation Matrix Constructor
+- [ ] Maybe a Vector object?
+	- [ ] With cross and dot products?
 
 # Build (Linux)
-Download the lua_matrix.c file and with gcc run
+Download the lua\_matrix.c file and with gcc run
 ```
 $ gcc -shared -o lua_matrix.so -fPIC lua_matrix.c
 ```
--- lua_matrix.c
+++ lua_matrix.c
@@ -5,6 +5,8 @@
 #include<lauxlib.h>
 
 #include<stdlib.h>
+#include<stdbool.h>
+
 #include<math.h>
 
 #define METATABLE "Matrix"
@@ -16,8 +18,8 @@ typedef struct Matrix {
 } Matrix;
 
 
-static Matrix *is_matrix(lua_State *L) {
-	void *ud = luaL_checkudata(L, 1, METATABLE);
+static Matrix *is_matrix(lua_State *L, int index) {
+	void *ud = luaL_checkudata(L, index, METATABLE);
 	luaL_argcheck(L, ud != NULL, 1, "`matrix' expected");
 	return (Matrix *)ud;
 }
@@ -50,7 +52,7 @@ static int make_matrix(lua_State *L) {
 
 // util function for set and get
 static double *get_element_addr(lua_State *L) {
-	Matrix *m = is_matrix(L);
+	Matrix *m = is_matrix(L, 1);
 	int row = luaL_checkinteger(L, 2);
 	int col = luaL_checkinteger(L, 3);
 
@@ -80,7 +82,7 @@ static int get_matrix_element(lua_State *L) {
 
 // accessing the size in lua
 static int get_size(lua_State *L) {
-	Matrix *m = is_matrix(L);
+	Matrix *m = is_matrix(L, 1);
 	lua_pushnumber(L, m->rows);
 	lua_pushnumber(L, m->cols);
 	return 2;
@@ -88,6 +90,39 @@ static int get_size(lua_State *L) {
 
 // }}}
 
+//{{{ Metamethods
+
+static bool same_size(Matrix *m1, Matrix *m2) {
+	return m1->rows == m2->rows && m1->cols == m2->cols; 
+}
+
+static int matrix_add(lua_State *L) {
+	// check if the two arguments are matrices and assign them
+	Matrix *m1 = is_matrix(L, 1);
+	Matrix *m2 = is_matrix(L, 2);
+	
+	// check if m1 and m2 are the same size
+	luaL_argcheck(L, same_size(m1, m2), 2, "Matrices not the same size");
+	
+	// make a new matrix to return
+	size_t nbytes = sizeof(Matrix) + (m1->rows * m1->cols) * sizeof(double);
+	Matrix *sum = (Matrix *)lua_newuserdata(L, nbytes);
+	
+	// set the size of it
+	sum->rows = m1->rows;
+	sum->cols = m1->cols;
+
+	// actually do the adding
+	for(int i = 0; i < m1->rows * m1->cols - 1; i++)
+		sum->val[i] = m1->val[i] + m2->val[i];
+
+	// set the metatable for the sum
+	luaL_setmetatable(L, METATABLE);
+
+	return 1; //return the matrix
+}
+
+//}}}
 
 // initialize the library
 static const struct luaL_reg matrixlib_f [] = {
@@ -95,25 +130,33 @@ static const struct luaL_reg matrixlib_f [] = {
 	{NULL, NULL}
 };
 
-// metamethods for matrix object
-static const struct luaL_reg matrixlib_m [] = {
+// default methods for matrix object
+static const struct luaL_reg matrixlib_m_index [] = {
 	{"set", set_matrix_element},
 	{"get", get_matrix_element},
+	{NULL, NULL}
+};
+
+// metamethods for matrix object
+static const struct luaL_reg matrixlib_metam [] = {
+	{"__add", matrix_add},
 	{NULL, NULL}
 };
 
 // this basically get called when require("lua_matrix") is called
 int luaopen_lua_matrix(lua_State *L) {
 	luaL_newmetatable(L, METATABLE);
-	lua_pushstring(L, "__index");
-	lua_pushvalue(L, -2);
-	lua_settable(L, -3);
+	// make the .__index entry in the metatable
+	lua_pushstring(L, "__index");		// push the key __index onto the stack
+	lua_newtable(L); 			// push a new empty table onto the stack
+	luaL_setfuncs(L, matrixlib_m_index, 0); // fill it with the index methods
+	lua_settable(L, -3); 			// METATABLE["__index"] = matrix_m_index
+	// make the rest of the metatable (__add and stuff)
+	luaL_setfuncs(L, matrixlib_metam, 0);
 
-	luaL_setfuncs(L, matrixlib_m, 0);
-
-	lua_newtable(L);
-	luaL_setfuncs(L, matrixlib_f, 0);
-	return 1;
+	lua_newtable(L); 			// push an empty table onto the stack
+	luaL_setfuncs(L, matrixlib_f, 0); 	// fill it with our functions
+	return 1;				// return it to lua
 }
 
 
